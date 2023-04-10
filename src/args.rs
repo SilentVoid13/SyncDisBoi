be6e8671a79bbfa8f2c5bf697e8cd17ec400e282
SilentVoid13 <51264226+SilentVoid13@users.noreply.github.com>
2023-04-10 23:22:37 +0200

    feat: Updating clap to v4

--- src/args.rs
+++ src/args.rs
@@ -2,14 +2,18 @@ use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(long, env, required(true))]
+    /// The secret for the YouTube Music API
+    #[arg(long, env, required = true)]
     pub yt_music_secret: String,
-    #[clap(long, env, required(true))]
+    /// The path to the cookies file for the YouTube Music API
+    #[arg(long, env, required = true)]
     pub yt_music_cookies: PathBuf,
-    #[clap(long, env, required(true))]
+    /// The client ID for the Spotify API application
+    #[arg(long, env, required = true)]
     pub spotify_client_id: String,
-    #[clap(long, env, required(true))]
+    /// The client secret for the Spotify API application
+    #[arg(long, env, required = true)]
     pub spotify_client_secret: String,
 }
