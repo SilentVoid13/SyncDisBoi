1bcb843a63f9481c862d011a3a61daed7767a887
silentvoid13 <51264226+SilentVoid13@users.noreply.github.com>
2024-10-10 10:26:11 +0200

    build: trying out cleaner flake

--- flake.nix
+++ flake.nix
@@ -4,99 +4,129 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flakebox = {
-      url = "github:SilentVoid13/flakebox";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = {
+    self,
     nixpkgs,
     flake-utils,
-    flakebox,
-    ...
+    naersk,
+    fenix,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
 
-        shellPackages = with pkgs; [
-          python311Packages.ytmusicapi
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.rustc
+            minimal.cargo
+            targets.x86_64-unknown-linux-musl.latest.rust-std
+            targets.x86_64-pc-windows-gnu.latest.rust-std
+            targets.aarch64-unknown-linux-musl.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        naerskBuildPackage = args:
+          naersk'.buildPackage (
+            args
+            // cargoConfig
+          );
+        naerskBuildPackageT = target: args: naerskBuildPackage (args // {CARGO_BUILD_TARGET = target;});
+
+        cargoConfig = {
+          # Tells Cargo to enable static compilation.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerustflags)
+          #
+          # Note that the resulting binary might still be considered dynamically
+          # linked by ldd, but that's just because the binary might have
+          # position-independent-execution enabled.
+          # (see: https://github.com/rust-lang/rust/issues/79624#issuecomment-737415388)
+          CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-feature=+crt-static";
+
+          # Tells Cargo that it should use Wine to run tests.
+          # (https://doc.rust-lang.org/cargo/reference/config.html#targettriplerunner)
+          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_RUNNER = pkgs.writeScript "wine-wrapper" ''
+            export WINEPREFIX="$(mktemp -d)"
+            exec wine64 $@
+          '';
+        };
+
+        fnBuildInputs = pkgs: with pkgs; [openssl];
+        shellPkgs = with pkgs; [
+          wineWowPackages.stable
         ];
-
-        flakeboxLib = flakebox.lib.${system} {
-          config = {
-            github.ci.enable = false;
-            just.enable = false;
-            convco.enable = false;
-            git.commit-msg.enable = false;
-            git.commit-template.enable = false;
-            git.pre-commit.enable = false;
-
-            env.shellPackages = shellPackages;
-          };
+      in rec {
+        defaultPackage = naerskBuildPackage {
+          src = ./.;
+          buildInputs =
+            (fnBuildInputs pkgs)
+            ++ (with pkgs;
+              lib.optionals stdenv.isDarwin
+              [
+                darwin.apple_sdk.frameworks.CoreFoundation
+                darwin.apple_sdk.frameworks.CoreServices
+                darwin.apple_sdk.frameworks.SystemConfiguration
+                darwin.apple_sdk.frameworks.Security
+              ]);
         };
-        project_name = "SyncDisBoi";
 
-        buildPaths = [
-          "Cargo.toml"
-          "Cargo.lock"
-          "src"
-        ];
+        packages.x86_64-unknown-linux-musl = naerskBuildPackageT "x86_64-unknown-linux-musl" {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [pkgsStatic.stdenv.cc];
+          buildInputs = fnBuildInputs pkgs.pkgsCross.musl64.pkgsStatic;
+        };
 
-        buildInputs = pkgs:
-          with pkgs;
-          # if the target is windows, we don't use the musl static openssl
-            (
-              if (stdenv.isLinux || stdenv.isDarwin)
-              then [
-                pkgsStatic.openssl
-              ]
-              else [openssl]
-            )
-            ++ lib.optionals stdenv.isDarwin
-            [
-              darwin.apple_sdk.frameworks.CoreFoundation
-              darwin.apple_sdk.frameworks.CoreServices
-              darwin.apple_sdk.frameworks.SystemConfiguration
-              darwin.apple_sdk.frameworks.Security
+        packages.aarch64-unknown-linux-musl = with pkgs.pkgsCross.aarch64-multiplatform-musl;
+          naerskBuildPackageT "aarch64-unknown-linux-musl" {
+            src = ./.;
+            strictDeps = true;
+            depsBuildBuild = [
+              stdenv.cc
             ];
-
-        nativeBuildInputs = pkgs:
-          with pkgs;
-            [
-              pkgsBuildTarget.pkg-config
-            ];
-
-        buildSrc = flakeboxLib.filterSubPaths {
-          root = builtins.path {
-            name = project_name;
-            path = ./.;
+            "CC_aarch64_unknown_linux_musl" = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+            "CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER" = "${stdenv.cc}/bin/${stdenv.cc.targetPrefix}cc";
+            buildInputs = fnBuildInputs pkgsStatic;
           };
-          paths = buildPaths;
+
+        packages.x86_64-pc-windows-gnu = naerskBuildPackageT "x86_64-pc-windows-gnu" {
+          src = ./.;
+          strictDeps = true;
+          depsBuildBuild = with pkgs; [
+            pkgsCross.mingwW64.stdenv.cc
+            pkgsCross.mingwW64.windows.pthreads
+          ];
+          nativeBuildInputs = with pkgs; [
+            # We need Wine to run tests:
+            wineWowPackages.stable
+          ];
+          buildInputs = fnBuildInputs pkgs.pkgsCross.mingwW64;
         };
 
-        multiBuild =
-          (flakeboxLib.craneMultiBuild {
-            inherit buildInputs;
-            inherit nativeBuildInputs;
-          }) (craneLib': let
-            craneLib = craneLib'.overrideArgs {
-              pname = project_name;
-              src = buildSrc;
-            };
-          in {
-            ${project_name} = craneLib.buildPackage {};
-          });
-      in {
-        packages.default = multiBuild.${project_name};
-
-        legacyPackages = multiBuild;
-
-        devShells = flakeboxLib.mkShells {
-          buildInputs = buildInputs pkgs;
-          nativeBuildInputs = nativeBuildInputs pkgs;
-        };
+        devShell = pkgs.mkShell (
+          {
+            inputsFrom = [defaultPackage];
+            packages = shellPkgs;
+            #CARGO_BUILD_TARGET = "x86_64-unknown-linux-gnu";
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (fnBuildInputs pkgs)}";
+          }
+          // cargoConfig
+        );
       }
     );
 }
